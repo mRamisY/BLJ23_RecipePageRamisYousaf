a447044e531e933c6103e6ee80ad780abdb7c45b
mRamisY <145563869+mRamisY@users.noreply.github.com>
2024-01-04 16:47:50 +0100

    toggle added

--- Cook/little.js
+++ Cook/little.js
@@ -1,4 +1,22 @@
+
+/*js mostly internet*/
+
 function toggleIngredient(checkbox) {
-    const ingredient = checkbox.parentNode;
-    ingredient.classList.toggle("completed", checkbox.checked);
+  const ingredient = checkbox.parentNode;
+  ingredient.classList.toggle("completed", checkbox.checked);
 }
+
+function toggleDim(checkboxId) {
+  var completionCheckbox = document.getElementById(checkboxId);
+  var instruction = completionCheckbox.closest(".instruction");
+
+  if (completionCheckbox.checked) {
+    instruction.classList.add("dimmed");
+  } else {
+    instruction.classList.remove("dimmed");
+  }
+}
+
+
+
+
