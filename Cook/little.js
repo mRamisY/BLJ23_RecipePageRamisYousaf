99254fcefe03ec2823cb08b6fb9b2ebe5a1eacf1
mRamisY <145563869+mRamisY@users.noreply.github.com>
2024-01-04 20:15:22 +0100

    alle korrekturen plus pdf

--- Cook/little.js
+++ Cook/little.js
@@ -16,7 +16,21 @@ function toggleDim(checkboxId) {
     instruction.classList.remove("dimmed");
   }
 }
+ function downloadPDF() {
+   // Erstelle ein unsichtbares <a>-Element
+   var link = document.createElement("a");
+   link.href = "griddy.pdf"; // Der Dateiname für die PDF-Datei
+   link.download = "TheCookers_Grittibänze.pdf"; // Der Name, den die heruntergeladene Datei haben soll
 
+   // Füge das <a>-Element zum DOM hinzu
+   document.body.appendChild(link);
+
+   // Simuliere einen Klick auf das <a>-Element
+   link.click();
+
+   // Entferne das <a>-Element, nachdem der Download abgeschlossen ist
+   document.body.removeChild(link);
+ }
 
 
 
